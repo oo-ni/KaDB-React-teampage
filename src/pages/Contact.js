e3e0834594f2385f0e7522051f7f1dfb7c53ad4b
asasia1935 <sungmin1935@naver.com>
2023-09-22 18:02:07 +0900

    KSM 2023/09/22 edit contact
    contact 파일 수정하였습니다

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,37 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import img1 from '../img/1.jpeg';
-import img2 from '../img/2.jpeg';
-import img3 from '../img/3.jpeg';
+import { Col, Container, Row } from 'react-bootstrap';
+import gitlogo from '../img/gitlogo.png';
+import "./KakaoFont.css"
+import imgkim from '../img/imgkim.jpg'
+import imgchae from '../img/imgchae.jpg'
+import imgyoon from '../img/imgyoon.jpg'
+import imgjang from '../img/imgjang.png'
+import imgkang from '../img/imgkang.jpg'
 
 const styles = {
-
-  customhr: {
-
-    width: 4,
-    border: '0px',
-    color: 'red',
-    backgroundColor: 'red',
-
-
-  },
-
   container: {
     margin: '15px',
     backgroundColor: '#bbb0dc',
     display: 'flex',
+    textAlign: 'center',
     borderRadius: 15,
   },
-  imageContainer: {
-    flex: 1,
-    padding: '15px',
-  },
-  textContainer: {
-    paddingTop: '15%',
+  teamMemberContainer: {
+    fontFamily: "KakaoRegular",
+    paddingTop: '9%',
     textAlign: 'center',
-    flex: 1,
     padding: '20px',
-    fontFamily: 'mainText',
-
+  },
+  faceImage: {
+    height: '260px',
   },
   image: {
     width: '100%',
@@ -40,47 +31,95 @@ const styles = {
   },
 };
 
+const teammembers = [
+  {
+    namedepart: "윤건우/컴퓨터공학과",
+    img: imgyoon,
+    email: "ndbsrjsdn@naver.com",
+    git: "https://github.com/Y-gw",
+    mbti: "ISTP",
+    word: "일어나 코딩해야지..",
+  },
+  {
+    namedepart: "김성민/소프트웨어학과",
+    img: imgkim,
+    email: "sungmin1935@naver.com",
+    git: "https://github.com/asasia1935",
+    mbti: "ESTP",
+    word: "배우고 성장하자!",
+  },
+  {
+    namedepart: "강석훈/컴퓨터공학과",
+    img: imgkang,
+    email: "hamer1233@naver.com",
+    git: "https://github.com/Seok-Hun",
+    mbti: "ISTP",
+    word: "그래도 해야하지 어쩌겠습니까",
+  },
+  {
+    namedepart: "채호기/소프트웨어학과",
+    img: imgchae,
+    email: "hogi1216@naver.com",
+    git: "https://github.com/hogishim",
+    mbti: "ISFJ",
+    word: "화이팅!!",
+  },
+  {
+    namedepart: "장현화/컴퓨터공학과",
+    img: imgjang,
+    email: "ex@example.com",
+    git: "ex@example.com",
+    mbti: "~~~~",
+    word: "한마디",
+  },
+]
+
 function Contact() {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-  // 이미지 변경 함수
-  const changeImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % 3);
-  };
-
-  // 3초 간격으로 이미지 변경
-  useEffect(() => {
-    const interval = setInterval(changeImage, 3000);
-
-    // 컴포넌트가 언마운트될 때 인터벌 제거
-    return () => clearInterval(interval);
-  }, []);
-
-  // 이미지 배열
-  const images = [img1, img2, img3];
-
   return (
-    <div style={styles.container}>
-      <div style={styles.imageContainer}>
-        <img
-          style={styles.image}
-          src={images[currentImageIndex]}
-          alt={`Image ${currentImageIndex + 1}`}
-        />
+    <div>
+      <div style={styles.teamMemberContainer} className='kakaofont'>
+        <Container>
+          <Row>
+            {teammembers.map((member, index) => {
+              if (index > 2) return false;
+              else return (
+                <Col>
+                  <div key={index}>
+                    <img style={styles.faceImage} src={member.img} />
+                    <div style={{ fontSize: '20px' }}>
+                      <a href={member.git}><img src={gitlogo} width='40px' /></a>
+                      &nbsp;{member.namedepart}
+                    </div>
+                    <p>{member.email}</p>
+                    <p>{member.mbti}</p>
+                    <p>{member.word}</p>
+                  </div>
+                </Col>
+              )
+            })}
+          </Row>
+          <Row>
+            {teammembers.map((member, index) => {
+              if (index < 3) return false;
+              else return (
+                <Col>
+                  <div key={index}>
+                    <img style={styles.faceImage} src={member.img} />
+                    <div style={{ fontSize: '20px' }}>
+                      <a href={member.git}><img src={gitlogo} width='40px' /></a>
+                      &nbsp;{member.namedepart}
+                    </div>
+                    <p>{member.email}</p>
+                    <p>{member.mbti}</p>
+                    <p>{member.word}</p>
+                  </div>
+                </Col>
+              )
+            })}
+          </Row>
+        </Container>
       </div>
-      <hr style={styles.customhr} />
-      <div style={styles.textContainer}>
-        <h2>About us</h2>
-        <h6>
-        <p>We are students from Gachon University
-          We are studying React, Spring Boot, HTML, JavaScript, CSS in Kakao Software
-          Academy class 'Web application development'</p>
-        <p>We are using Slack, Notion, KakaoWork,
-          Kakao Azit as our colloaboration tool</p>
-        <p>We are planning to make a blog as our group project</p>
-        </h6>
-      </div>
-    </div>
+    </div >
   );
 }
 
